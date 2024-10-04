0a46e53162c7a919a79b5bca5637c1251abda5b6
StellaStar1 <167599143+StellaStar1@users.noreply.github.com>
2024-10-04 20:40:32 +1000

    implemented key variables
    variables corresponding to number of employees and time limit for the day

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,6 +10,10 @@ public class GameManager : MonoBehaviour
     public Character character;
 
     public int score;
+    public int remaining;
+    public int fails;
+    public float timer;
+    public float timeLimit = 300f;
 
     public TMP_Text scoreText;
 
@@ -22,27 +26,40 @@ public class GameManager : MonoBehaviour
 
     public AudioSource popSound;
 
+    public int dayCounter = 1;
+
+    private bool gameRunning = true;
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
+        gameRunning = true;
+        StartDay();
         GenerateNewCharacter();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E)) Accept();
-        if (Input.GetKeyDown(KeyCode.Q)) Deny();
-        scoreText.text = "Score: " + score;
-        ID.gameObject.SetActive(anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"));
+        if (gameRunning)
+        {
+            if (Input.GetKeyDown(KeyCode.E)) Accept();
+            if (Input.GetKeyDown(KeyCode.Q)) Deny();
+            scoreText.text = "Score: " + score;
+            ID.gameObject.SetActive(anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"));
+
+            if (Time.time >= timer + timeLimit)
+            {
+                Debug.Log("Game Over");
+                gameRunning = false;
+            }
+        }
     }
 
     public void Accept()
     {
         popSound.Play();
-        if (character.valid) score += 100;
-        else score -= 100;
+        if (character.valid) score++;
+        else fails++;
         anim.SetTrigger("Accept");
         Destroy(charSprite, 0.30f);
         GenerateNewCharacter();
@@ -51,8 +68,8 @@ public class GameManager : MonoBehaviour
     public void Deny()
     {
         popSound.Play();
-        if (!character.valid) score += 100;
-        else score -= 100;
+        if (!character.valid) score++;
+        else fails++;
         anim.SetTrigger("Deny");
         Destroy(charSprite, 0.30f);
         GenerateNewCharacter();
@@ -71,5 +88,13 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(seconds);
         charSprite = Instantiate(charSprites[Random.Range(0, charSprites.Length)], new Vector3(-12, 0, 0), Quaternion.identity);
         anim = charSprite.GetComponent<Animator>();
+    }
+
+    public void StartDay()
+    {
+        timer = Time.time;
+        fails = 0;
+        score = 0;
+        remaining = 10;
     }
 }
