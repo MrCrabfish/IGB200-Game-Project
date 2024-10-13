870614b3390ab54b20f0261e36e4c860116fdc8f
Calex <conalexander74@gmail.com>
2024-10-13 20:28:40 +1000

    Continued work on Tutorial

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,22 +6,33 @@ using UnityEngine.SceneManagement;
 public class Scenes : MonoBehaviour
 {
     public AudioSource popSound;
+    private bool tutorialLoaded = false;  // Variable to track if the tutorial has already been loaded
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return)) StartGame();
+        // Check if the Enter key is pressed
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartGame();
+        }
     }
 
     public void StartGame()
     {
+        // Play the pop sound effect
         popSound.Play();
-        SceneManager.LoadScene(0);
+
+        // Get the current active scene
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // Check if we are currently in the tutorial scene
+        if (currentScene.buildIndex == 0) // Assuming the tutorial is Scene 0
+        {
+            SceneManager.LoadScene(2); // Load the gameplay scene (Scene 2)
+        }
+        else if (currentScene.buildIndex == 1) // Assuming the title screen is Scene 1
+        {
+            SceneManager.LoadScene(0); // Load the tutorial scene (Scene 0)
+        }
     }
 }
