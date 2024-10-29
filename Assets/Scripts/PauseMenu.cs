f72fcdc2ee954d94dd678ff30f7414818f1afe1b
MrCrabfish <nicaburda@gmail.com>
2024-10-29 11:03:55 +1000

    various
    font changes, fixed computer ui in tutorial scene, and added pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
